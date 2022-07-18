d320464a83fd57bcc907cc427033914e55d7b772
mbaykenar <aykenar.burak@gmail.com>
2022-07-18 16:57:26 +0300

    change config.tcl

--- openlane/pulpino_top_wrapper/config.tcl
+++ openlane/pulpino_top_wrapper/config.tcl
@@ -225,26 +225,27 @@ set ::env(DESIGN_IS_CORE) 0
 
 set ::env(CLOCK_PORT) "wb_clk_i"
 set ::env(CLOCK_NET) "pulpino_top_wrapper.clk"
-set ::env(CLOCK_PERIOD) "100"
+set ::env(CLOCK_PERIOD) "200"
 
-set ::env(PL_RESIZER_MAX_SLEW_MARGIN) 20
-set ::env(PL_RESIZER_MAX_CAP_MARGIN) 20
-set ::env(GLB_RESIZER_MAX_SLEW_MARGIN) 10
+set ::env(PL_RESIZER_MAX_SLEW_MARGIN) 40
+set ::env(PL_RESIZER_MAX_CAP_MARGIN) 40
+set ::env(GLB_RESIZER_MAX_SLEW_MARGIN) 40
 
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 2500 2500"
 
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
-set ::env(SYNTH_STRATEGY) "AREA 0"
+set ::env(SYNTH_STRATEGY) "AREA 1"
 set ::env(PL_BASIC_PLACEMENT) 0
-set ::env(PL_TARGET_DENSITY) 0.35
-set ::env(FP_CORE_UTIL) {40}
-set ::env(PL_MACRO_CHANNEL) {20 20}
+set ::env(PL_TARGET_DENSITY) 0.3
+set ::env(FP_CORE_UTIL) {30}
+set ::env(PL_MACRO_CHANNEL) {30 30}
 set ::env(PL_MACRO_HALO) {10 10}
-set ::env(CELL_PAD) {1}
+set ::env(CELL_PAD) {2}
 set ::env(GLB_RESIZER_HOLD_MAX_BUFFER_PERCENT) {60}
-set ::env(ROUTING_CORES) {4}
-set ::env(GLB_RT_OVERFLOW_ITERS) {64}
+set ::env(ROUTING_CORES) {6}
+set ::env(GLB_RT_OVERFLOW_ITERS) {100}
+set ::env(GLB_RT_ALLOW_CONGESTION) 1
 
 ### Macro Placement
 set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
@@ -256,7 +257,7 @@ set ::env(MACRO_PLACEMENT_CFG) $script_dir/macro.cfg
 # 
 # set ::env(GLB_RT_MAXLAYER) 5
 
-set ::env(RT_MAX_LAYER) {met4}
+set ::env(RT_MAX_LAYER) {met5}
 
 # You can draw more power domains if you need to 
 set ::env(VDD_NETS) [list {vccd1}]
